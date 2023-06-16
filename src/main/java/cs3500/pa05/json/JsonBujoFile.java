b4e0dae2fd07b7db58f0a95c46904a3e2e5de84b
Liang jimenez <liangjimenez@yahoo.com>
2023-06-16 18:06:00 -0400

    Changes by meaney.an

--- src/main/java/cs3500/pa05/json/JsonBujoFile.java
+++ src/main/java/cs3500/pa05/json/JsonBujoFile.java
@@ -1,6 +1,7 @@
 package cs3500.pa05.json;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 
 public record JsonBujoFile(
     @JsonProperty ("Max Tasks") int maxTasks,
