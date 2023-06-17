70b2b2d64bfad52afbee1ef5bddd60b44f51709f
LoganStanhope <lastanhope@outlook.com>
2023-06-17 15:57:55 -0400

    Welcome screen works with menu bar having all its attributes.

--- src/main/java/cs3500/pa05/controller/WeekViewController.java
+++ src/main/java/cs3500/pa05/controller/WeekViewController.java
@@ -1,15 +1,24 @@
 package cs3500.pa05.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+
 /**
  * Handles interaction with the week view between the user and the model representation
  */
 public class WeekViewController extends AbstractController {
+
+//  @FXML
+//  private Button viewMenu;
 
   /**
    * Runs the JavaFX scene based on a week's data of tasks and events
    */
   @Override
   public void run() {
-
+    //TODO: action to make the side bar show up
+//    viewMenu.setOnAction(e -> createNewFile.setContentDisplay(
+//        ContentDisplay.valueOf("weekView.fxml")));
   }
 }
