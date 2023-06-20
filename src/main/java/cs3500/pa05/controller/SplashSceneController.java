848f5801d8b61b4fc09f3109857e169bdd446931
Liang jimenez <liangjimenez@yahoo.com>
2023-06-20 11:15:36 -0400

    Changes by meaney.an

--- src/main/java/cs3500/pa05/controller/SplashSceneController.java
+++ src/main/java/cs3500/pa05/controller/SplashSceneController.java
@@ -2,8 +2,13 @@ package cs3500.pa05.controller;
 
 import java.io.FileNotFoundException;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 public class SplashSceneController extends AbstractController {
   @FXML
@@ -12,11 +17,22 @@ public class SplashSceneController extends AbstractController {
   @FXML
   Button hidden;
 
+  Scene gifLoaded;
+
   @Override
   public void run() {
-    //Image image1 =
-    //    new Image(SplashSceneController.class.getResourceAsStream("Bullet Jouraling.gif"));
-    //bulletJournalLogo.setImage(image1);
+    InputStream stream = null;
+    try {
+      stream = new FileInputStream("src/main/resources/bujo.gif");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+    Image image = new Image(stream);
+    bulletJournalLogo.setImage(image);
+    bulletJournalLogo.setPreserveRatio(true);
+
+    Group root = new Group(bulletJournalLogo);
+    gifLoaded = new Scene(root, 595, 370);
 
     try {
       Thread.sleep(2000);
@@ -27,4 +43,8 @@ public class SplashSceneController extends AbstractController {
     switchScene(hidden,
         new WelcomeController(), "welcomePage.fxml");
   }
+
+  public Scene returnGif() {
+    return gifLoaded;
   }
+}
