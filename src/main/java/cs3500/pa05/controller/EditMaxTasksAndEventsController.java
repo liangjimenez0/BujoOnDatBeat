518c145019d252c5f2f2eb7e77f011d47bfa2635
LoganStanhope <lastanhope@outlook.com>
2023-06-20 00:20:04 -0400

    more stuff changed

---
+++ src/main/java/cs3500/pa05/controller/EditMaxTasksAndEventsController.java
@@ -0,0 +1,33 @@
+package cs3500.pa05.controller;
+
+import cs3500.pa05.model.Week;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditMaxTasksAndEventsController extends AbstractController {
+
+  @FXML
+  private TextField maxTasksInput, maxEventsInput;
+
+  @FXML
+  private Button doneButton, backButton;
+
+  private Week week;
+
+  public EditMaxTasksAndEventsController(Week week) {
+    this.week = week;
+  }
+
+  public void run() {
+    doneButton.setOnAction(e -> editMax());
+    backButton.setOnAction(e -> switchScene(backButton, new WeekViewController(this.week), "weekView.fxml"));
+  }
+
+  private void editMax() {
+    this.week.changeMaxTasks(Integer.parseInt(maxTasksInput.getText()));
+    this.week.changeMaxEvents(Integer.parseInt(maxEventsInput.getText()));
+
+    switchScene(backButton, new WeekViewController(this.week), "weekView.fxml");
+  }
+}
