59f8405b84ffb75ced088b9d52bc019567c0b9f4
LoganStanhope <lastanhope@outlook.com>
2023-06-21 11:47:34 -0400

    a couple test classes have the FXML testing

--- src/test/java/cs3500/pa05/controller/WeekViewControllerTest.java
+++ src/test/java/cs3500/pa05/controller/WeekViewControllerTest.java
@@ -1,36 +1,51 @@
 package cs3500.pa05.controller;
 
+import cs3500.pa05.model.Week;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-public class WeekViewControllerTest {
 
-  @ExtendWith(ApplicationExtension.class)
-  class ChangeWeekNameAction {
+@ExtendWith(ApplicationExtension.class)
+class WeekViewControllerTest {
+  private Button button;
+  private WeekViewController controller;
+  private Week week;
 
-    /**
-     * Will be called with {@code @Before} semantics, i. e. before each test method.
-     *
-     * @param stage - Will be injected by the test runner.
-     */
-    @Start
-    private void start(Stage stage) {}
-
-    @Test
-    private void testChangeWeekName(FxRobot robot) {}
-
+  /**
+   * Will be called with {@code @Before} semantics, i. e. before each test method.
+   *
+   * @param stage - Will be injected by the test runner.
+   */
+  @Start
+  void start(Stage stage) {
+    button = new Button("create new file");
+    button.setId("createNewFile");
+    week = new Week(1, 1, "filename", "password");
+    button.setOnAction(actionEvent -> controller.switchScene(button,
+        new CreateNewFileController(), "createNewFile.fxml"));
+    controller = new WeekViewController(week);
+    stage.setScene(new Scene(new StackPane(button), 100, 100));
+    stage.show();
   }
 
-
   @Test
-  void switchScene() {
-  }
-
-  @Test
-  void run() {
+  void testChangeWeekNameButton(FxRobot robot) {
+    controller.run();
+    Assertions.assertThat(button).hasText("create new file");
+    Assertions.assertThat(robot.lookup("#createNewFile").queryAs(Button.class))
+        .hasText("createNewFile");
+    Assertions.assertThat(robot.lookup("create new file").queryAs(Button.class))
+        .hasText("create new file");
+    Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("create new file");
+    controller.switchScene(button, controller, "createNewFile");
   }
 }
+
--- src/test/java/cs3500/pa05/controller/WelcomeControllerTest.java
+++ src/test/java/cs3500/pa05/controller/WelcomeControllerTest.java
@@ -1,36 +1,49 @@
 package cs3500.pa05.controller;
 
+import cs3500.pa05.model.Week;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
+
+@ExtendWith(ApplicationExtension.class)
 class WelcomeControllerTest {
+  private Button button;
+  private WelcomeController controller = new WelcomeController();
 
-  @ExtendWith(ApplicationExtension.class)
-  class ChangeWeekNameAction {
-
-    /**
-     * Will be called with {@code @Before} semantics, i. e. before each test method.
-     *
-     * @param stage - Will be injected by the test runner.
-     */
-    @Start
-    private void start(Stage stage) {}
-
-    @Test
-    private void testChangeWeekName(FxRobot robot) {}
-
+  /**
+   * Will be called with {@code @Before} semantics, i. e. before each test method.
+   *
+   * @param stage - Will be injected by the test runner.
+   */
+  @Start
+  void start(Stage stage) {
+    button = new Button("open existing file");
+    button.setId("openExistingFile");
+    Week week = new Week(1, 1, "filename", "password");
+    button.setOnAction(actionEvent -> controller.switchScene(button,
+        new OpenExistingFileController(), "openExistingFile.fxml"));
+    stage.setScene(new Scene(new StackPane(button), 100, 100));
+    stage.show();
   }
 
   @Test
-  void switchScene() {
-  }
-
-  @Test
-  void run() {
+  void testChangeWeekName(FxRobot robot) {
+    controller.run();
+    Assertions.assertThat(button).hasText("create new file");
+    Assertions.assertThat(robot.lookup("#openExistingFile").queryAs(Button.class))
+        .hasText("open existing file");
+    Assertions.assertThat(robot.lookup("create new file").queryAs(Button.class))
+        .hasText("open existing file");
+    Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("click me!");
+    Assertions.assertThat(button).hasText("create new file");
+    controller.switchScene(button, new OpenExistingFileController(), "openExistingFile.fxml");
   }
 }
